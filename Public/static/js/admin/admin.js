5ebe95fa27f2cc0bbdbb7a87ec03c913eea5477d
Iankoex <koexian@gmail.com>
2023-06-13 09:09:09 +0300

    working links and members view

--- Public/static/js/admin/admin.js
+++ Public/static/js/admin/admin.js
@@ -8,7 +8,25 @@ $('.delete-post-button').click(function(){
 
 $('#delete_tag_btn').click(function(){
     if (confirm('Are you sure you want to delete this tag?')) {
-        let tag_slug = $('#tag_slug').text()
         window.location.replace(`${window.location.href}delete`);
+    };
+})
+
+$('#gh-mobile-nav-bar-more').click(function(){
+    $('.gh-viewport').addClass('mobile-menu-expanded')
+})
+
+$('#gh-content-cover').click(function(){
+    $('.gh-viewport').removeClass('mobile-menu-expanded')
+})
+
+$('.gh-alerts').click(function(){
+    $('.gh-alerts').addClass('hidden')
+})
+
+$('#admin-user-account').click(function(){
+    if (confirm('Are you sure you want to logout?')) {
+        let siteURL = $('#site_url').text()
+        window.location.replace(`${siteURL}/steampress/logout/`);
     };
 })
